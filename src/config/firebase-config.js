9917738282c1bc8eec17958b834805db018d3032
Brian <briankariuki9975@gmail.com>
2025-12-31 13:16:38 +0300

    move vercel.json to root

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA6asFGtyi-JL7qbAK-ujSSIwfZ28_ce4g",
-  authDomain: "job-application-tracking-53e02.firebaseapp.com",
-  projectId: "job-application-tracking-53e02",
-  storageBucket: "job-application-tracking-53e02.firebasestorage.app",
-  messagingSenderId: "390189911550",
-  appId: "1:390189911550:web:b59883b9ccdfa8821604af"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
